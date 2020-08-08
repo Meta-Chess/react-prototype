3bdb2b2a39b5901313b9de65d81063821b0405a2
Angus <angussbjohnson@gmail.com>
2020-08-08 20:51:08 +1000

    Restrcture for pieces and glow

--- src/primitives/PieceImage/PieceImage.tsx
+++ src/primitives/PieceImage/PieceImage.tsx
@@ -1,30 +1,53 @@
 import React, { FC } from "react";
 import { PieceType } from "domain/Game/types";
+import { Svg, G } from "react-native-svg";
+import { Colors } from "primitives";
 import { Bishop, King, Knight, Pawn, Queen, Rook } from "./sprites";
 
 interface Props {
   type: PieceType;
   color: string;
   size: number;
+  glowColor?: string;
 }
 
-const PieceImage: FC<Props> = ({ type, color, size }) => {
-  switch (type) {
-    case PieceType.Pawn:
-      return <Pawn size={size} color={color} active={false} />;
-    case PieceType.Rook:
-      return <Rook size={size} color={color} active={false} />;
-    case PieceType.Knight:
-      return <Knight size={size} color={color} active={false} />;
-    case PieceType.Bishop:
-      return <Bishop size={size} color={color} active={false} />;
-    case PieceType.King:
-      return <King size={size} color={color} active={false} />;
-    case PieceType.Queen:
-      return <Queen size={size} color={color} active={false} />;
-    default:
-      return null;
-  }
+const PieceImage: FC<Props> = ({ type, color, size, glowColor }) => {
+  const primary = color;
+  const secondary = Colors.DARKEST.string();
+  const paths =
+    type === PieceType.Pawn ? (
+      <Pawn />
+    ) : type === PieceType.Bishop ? (
+      <Bishop />
+    ) : type === PieceType.Knight ? (
+      <Knight />
+    ) : type === PieceType.Rook ? (
+      <Rook />
+    ) : type === PieceType.Queen ? (
+      <Queen />
+    ) : (
+      <King />
+    );
+
+  const glowAlphas = [0.03, 0.1, 0.1, 0.2, 0.3, 0.4, 1, 1];
+  return (
+    <Svg width={size} height={size} viewBox="0 0 45 45">
+      {glowColor &&
+        glowAlphas.map((alpha, i) => (
+          <G
+            stroke={glowColor}
+            strokeWidth={9 - Math.sqrt(9 * i)}
+            strokeOpacity={alpha}
+            key={i}
+          >
+            {paths}
+          </G>
+        ))}
+      <G fill={primary} stroke={secondary} strokeWidth={0.9}>
+        {paths}
+      </G>
+    </Svg>
+  );
 };
 
 export { PieceImage };
--- src/primitives/PieceImage/sprites/Rook.tsx
+++ src/primitives/PieceImage/sprites/Rook.tsx
@@ -1,28 +1,17 @@
 import React, { FC } from "react";
-import { Svg, Path, G } from "react-native-svg";
-import { Colors } from "primitives";
+import { Path } from "react-native-svg";
 
-interface Props {
-  color: string;
-  active: boolean;
-  size: number;
-}
-
-const Rook: FC<Props> = ({ color, active, size }) => {
-  const primary = color;
-  const secondary = active ? color : Colors.DARKEST.string();
+const Rook: FC = () => {
   return (
-    <Svg width={size} height={size} viewBox="0 0 45 45">
-      <G fill={primary} stroke={secondary} strokeWidth={0.9}>
-        <Path d="M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 z " />
-        <Path d="M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 z " />
-        <Path d="M 11,14 L 11,9 L 15,9 L 15,11 L 20,11 L 20,9 L 25,9 L 25,11 L 30,11 L 30,9 L 34,9 L 34,14" />
-        <Path d="M 34,14 L 31,17 L 14,17 L 11,14" />
-        <Path d="M 31,17 L 31,29.5 L 14,29.5 L 14,17" />
-        <Path d="M 31,29.5 L 32.5,32 L 12.5,32 L 14,29.5" />
-        <Path d="M 11,14 L 34,14" />
-      </G>
-    </Svg>
+    <>
+      <Path d="M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 z " />
+      <Path d="M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 z " />
+      <Path d="M 11,14 L 11,9 L 15,9 L 15,11 L 20,11 L 20,9 L 25,9 L 25,11 L 30,11 L 30,9 L 34,9 L 34,14" />
+      <Path d="M 34,14 L 31,17 L 14,17 L 11,14" />
+      <Path d="M 31,17 L 31,29.5 L 14,29.5 L 14,17" />
+      <Path d="M 31,29.5 L 32.5,32 L 12.5,32 L 14,29.5" />
+      <Path d="M 11,14 L 34,14" />
+    </>
   );
 };
 
