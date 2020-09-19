ebceec3dcb7a24297f7b07926d86e75bf05a9c32
AngusLeck <aleck42@gmail.com>
2020-09-19 11:11:23 +1000

    small formating change

--- src/game/Rules/index.ts
+++ src/game/Rules/index.ts
@@ -1,3 +1,4 @@
+//START: named rules
 export * from "./Castling";
 export * from "./Cylindrical";
 export * from "./Hex";
@@ -8,6 +9,7 @@ export * from "./Polar";
 export * from "./Standard";
 export * from "./Check";
 export * from "./loseWithNoKings";
+export * from "./Fatigue";
+//END: named rules
 
 export * from "./Rules";
-export * from "./Fatigue";
