2af786be319294203811b17a9cf4d9f17217f55b
EmmanuelIsaac <58617353+EmmanuelIsaac@users.noreply.github.com>
2020-11-15 15:44:01 +1100

    lint fix

--- src/components/shared/Board/HexBoardBacking.tsx
+++ src/components/shared/Board/HexBoardBacking.tsx
@@ -14,7 +14,6 @@ const HexBoardBacking: SFC<HexBoardBackingProps> = ({
   padding,
   boardWidth,
   boardHeight,
-  shadow,
 }) => {
   const centerWidth = boardWidth;
   const endHalfWidth = boardWidth / 2;
