d70c0c208a0a3312bd4a1899941cd74022c69c88
EmmanuelIsaac <58617353+EmmanuelIsaac@users.noreply.github.com>
2020-12-24 14:40:40 +1100

    card tag tweaks

--- src/ui/Pressable/VariantTile/VariantTileInfo/TraitLabel.tsx
+++ src/ui/Pressable/VariantTile/VariantTileInfo/TraitLabel.tsx
@@ -2,28 +2,30 @@ import React from "react";
 import { SFC, Text } from "primitives";
 import { View } from "react-native";
 import { TraitClass, traitInfo } from "game/variants";
-import { contrast } from "utilities";
+import { opacify } from "utilities";
+import { Colors } from "primitives";
 
 interface TraitLabelProps {
   trait: TraitClass;
 }
 
 const TraitLabel: SFC<TraitLabelProps> = ({ trait, style }) => {
-  const color = traitInfo[trait].color.toString();
+  const color = opacify(traitInfo[trait].color.toString(), 0.5);
   return (
     <View
       style={[
         style,
         {
+          alignItems: "center",
           backgroundColor: color,
           borderRadius: 6,
-          width: 12,
-          height: 12,
+          paddingHorizontal: 7,
+          paddingVertical: 1,
         },
       ]}
     >
-      <Text cat={"BodyXS"} color={contrast(color)}>
-        {}
+      <Text cat={"BodyXS"} color={Colors.TEXT.LIGHT.toString()}>
+        {traitInfo[trait].name}
       </Text>
     </View>
   );
