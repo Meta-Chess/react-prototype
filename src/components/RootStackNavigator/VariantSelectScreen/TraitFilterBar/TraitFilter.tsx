3c4ee9ccfea586af299ae534467fa68a86243762
EmmanuelIsaac <58617353+EmmanuelIsaac@users.noreply.github.com>
2020-12-26 00:11:51 +1100

    tag filter final

--- src/components/RootStackNavigator/VariantSelectScreen/TraitFilterBar/TraitFilter.tsx
+++ src/components/RootStackNavigator/VariantSelectScreen/TraitFilterBar/TraitFilter.tsx
@@ -22,12 +22,26 @@ const TraitFilter: SFC<TraitFilterProps> = ({
 }) => {
   const baseColor = traitInfo[trait].color.fade(0.5);
   const color = selected ? baseColor : baseColor.fade(0.5);
+  const outerPadding = 8;
+  const innerPadding = 6;
+  const verticalPadding = 2;
   return (
-    <TouchableLabel style={style} color={color} onPress={onPress}>
+    <TouchableLabel
+      outerPadding={outerPadding}
+      innerPadding={innerPadding}
+      verticalPadding={verticalPadding}
+      style={style}
+      color={color}
+      onPress={onPress}
+    >
       <Text cat={"BodyS"} color={Colors.TEXT.LIGHT.toString()}>
         {trait}
       </Text>
-      <CountContainer>
+      <CountContainer
+        outerPadding={outerPadding}
+        innerPadding={innerPadding}
+        verticalPadding={verticalPadding}
+      >
         <Text cat={"BodyXS"} color={Colors.TEXT.LIGHT.toString()}>
           {numberOfVariantsWithTrait.toString()}
         </Text>
@@ -36,20 +50,30 @@ const TraitFilter: SFC<TraitFilterProps> = ({
   );
 };
 
-const TouchableLabel = styled(TouchableOpacity)<{ color: Color }>`
+const TouchableLabel = styled(TouchableOpacity)<{
+  color: Color;
+  outerPadding: number;
+  innerPadding: number;
+  verticalPadding: number;
+}>`
   flex-direction: row;
   border-radius: 6px;
-  padding-left: 8px;
-  margin-top: 1px;
+  padding-left: ${({ outerPadding }): number => outerPadding}px;
+  padding-vertical: ${({ verticalPadding }): number => verticalPadding}px;
   background-color: ${({ color }): string => color.string()};
   overflow: hidden;
 `;
 
-const CountContainer = styled(View)`
+const CountContainer = styled(View)<{
+  outerPadding: number;
+  innerPadding: number;
+  verticalPadding: number;
+}>`
   justify-content: center;
-  margin-left: 6px;
-  padding-left: 6px;
-  padding-right: 8px;
+  margin-vertical: -${({ verticalPadding }): number => verticalPadding}px;
+  margin-left: ${({ innerPadding }): number => innerPadding}px;
+  padding-left: ${({ innerPadding }): number => innerPadding}px;
+  padding-right: ${({ outerPadding }): number => outerPadding}px;
   background-color: ${Colors.BLACK.fade(0.75).toString()};
 `;
 
