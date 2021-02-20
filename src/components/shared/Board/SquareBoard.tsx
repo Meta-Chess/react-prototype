366e74e900a9664a500668f58279dc2122f1d5f6
Angus <angussbjohnson@gmail.com>
2021-02-20 21:22:44 +1100

    Board rotations

--- src/components/shared/Board/SquareBoard.tsx
+++ src/components/shared/Board/SquareBoard.tsx
@@ -1,52 +1,108 @@
-import React, { useContext } from "react";
-import { View } from "react-native";
+import React, { useContext, useState, useCallback, useEffect, useRef } from "react";
+import { View, Animated, Platform, Easing } from "react-native";
 import styled from "styled-components/native";
 import { SFC, Colors } from "primitives";
-import { objectMatches, range } from "utilities";
+import { objectMatches, range, wrapToCylinder } from "utilities";
 import { GameContext, SquareShape, TokenName } from "game";
 import { Square } from "./Square";
 import { BoardProps } from "components/shared/Board/Board";
 import { Styles } from "primitives/Styles";
 import { BoardMeasurements } from "components/shared";
+import { AbsoluteView } from "ui";
+
+const ROTATION_TIME = 50;
 
 const SquareBoard: SFC<BoardProps> = ({
   backboard = true,
   measurements,
   flipBoard = false,
 }) => {
+  const [verticalRotation, setVerticalRotation] = useState(0);
+  const [horizontalRotation, setHorizontalRotation] = useState(0);
+
+  const animationOffset = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+
+  const onKeyDownEvent = useCallback(
+    (event) => {
+      switch (event.key) {
+        case "w":
+          setVerticalRotation(verticalRotation + 1);
+          animationOffset.setValue({ x: 0, y: -2 * measurements.squareSize });
+          break;
+        case "a":
+          setHorizontalRotation(horizontalRotation - 1);
+          animationOffset.setValue({ x: 2 * measurements.squareSize, y: 0 });
+          break;
+        case "s":
+          setVerticalRotation(verticalRotation - 1);
+          animationOffset.setValue({ x: 0, y: 2 * measurements.squareSize });
+          break;
+        case "d":
+          setHorizontalRotation(horizontalRotation + 1);
+          animationOffset.setValue({ x: -2 * measurements.squareSize, y: 0 });
+          break;
+      }
+      Animated.timing(animationOffset, {
+        toValue: { x: 0, y: 0 },
+        duration: ROTATION_TIME,
+        easing: Easing.out(Easing.ease),
+        useNativeDriver: Platform.OS === "ios",
+      }).start();
+    },
+    [horizontalRotation, verticalRotation]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDownEvent, false);
+    return () => {
+      document.removeEventListener("keydown", onKeyDownEvent, false);
+    };
+  }, [onKeyDownEvent]);
+
   const { gameMaster } = useContext(GameContext);
   const game = gameMaster?.game;
   if (!game) return null;
 
   const { minRank, maxRank, minFile, maxFile } = measurements.rankAndFileBounds;
-  const fileCoordinates = range(minFile, maxFile - minFile + 1);
-  const rankCoordinates = range(minRank, maxRank - minFile + 1);
+  const fileCoordinates = range(minFile - 1, maxFile - minFile + 3);
+  const rankCoordinates = range(minRank - 1, maxRank - minFile + 3);
+
+  const horizontalWrap = wrapToCylinder(minFile, maxFile);
+  const verticalWrap = wrapToCylinder(minRank, maxRank);
 
   return (
-    <BoardContainer
-      measurements={measurements}
-      backboard={backboard}
-      style={{ flexDirection: flipBoard ? "row-reverse" : "row" }}
-    >
-      {fileCoordinates.map((file) => (
-        <ColumnContainer flipBoard={flipBoard} key={file}>
-          {rankCoordinates.map((rank) => (
-            <Square
-              size={measurements.squareSize}
-              shape={SquareShape.Square}
-              square={game.board.firstSquareSatisfyingRule(
-                (square) =>
-                  objectMatches({
-                    rank,
-                    file,
-                  })(square.coordinates) &&
-                  !square.hasTokenWithName(TokenName.InvisibilityToken)
-              )}
-              key={JSON.stringify([rank, file])}
-            />
+    <BoardContainer measurements={measurements} backboard={backboard}>
+      <AbsoluteView
+        style={{ overflow: "hidden", margin: measurements.boardPaddingHorizontal }}
+      >
+        <Animated.View
+          style={{
+            flexDirection: flipBoard ? "row-reverse" : "row",
+            marginLeft: animationOffset.x,
+            marginBottom: animationOffset.y,
+          }}
+        >
+          {fileCoordinates.map((file) => (
+            <ColumnContainer flipBoard={flipBoard} key={file}>
+              {rankCoordinates.map((rank) => (
+                <Square
+                  size={measurements.squareSize}
+                  shape={SquareShape.Square}
+                  square={game.board.firstSquareSatisfyingRule(
+                    (square) =>
+                      objectMatches({
+                        rank: verticalWrap(rank - verticalRotation),
+                        file: horizontalWrap(file - horizontalRotation),
+                      })(square.coordinates) &&
+                      !square.hasTokenWithName(TokenName.InvisibilityToken)
+                  )}
+                  key={JSON.stringify([rank, file])}
+                />
+              ))}
+            </ColumnContainer>
           ))}
-        </ColumnContainer>
-      ))}
+        </Animated.View>
+      </AbsoluteView>
     </BoardContainer>
   );
 };
