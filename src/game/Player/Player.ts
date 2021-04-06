e3efb8e15e5d126fd730fabb9052561241cbd7f6
Angus Leck <aleck42@gmail.com>
2021-04-06 20:05:03 +1000

    players are token owners

--- src/game/Player/Player.ts
+++ src/game/Player/Player.ts
@@ -1,26 +1,31 @@
-import { PlayerName } from "game/types";
+import { TokenOwner } from "game/Board/TokenOwner";
+import { PlayerName, Token } from "game/types";
 import { cloneDeep } from "lodash";
 import { PlayerRuleData, PlayerRuleSetterInput } from "./PlayerRuleData";
 
-export class Player {
-  private ruleData: PlayerRuleData = {};
+export class Player extends TokenOwner {
+  private ruleData: PlayerRuleData = {}; // TODO: is this being cloned/reset?
   public connected = false;
 
   constructor(
     public name: PlayerName,
     public alive: boolean = true,
+    public tokens: Token[] = [],
     public wantsToDraw: boolean = false,
     public endGameMessage: string = "",
     public hasLegalMoves: { value: boolean; turn: number } = {
       value: false,
       turn: -1,
     }
-  ) {}
+  ) {
+    super(tokens);
+  }
 
   clone(): Player {
     const cloneConstructorInput: Required<ConstructorParameters<typeof Player>> = [
       this.name,
       this.alive,
+      cloneDeep(this.tokens),
       this.wantsToDraw,
       this.endGameMessage,
       cloneDeep(this.hasLegalMoves),
@@ -30,6 +35,7 @@ export class Player {
 
   resetTo(savePoint: Player): void {
     this.name = savePoint.name;
+    this.tokens = cloneDeep(savePoint.tokens);
     this.alive = savePoint.alive;
     this.wantsToDraw = savePoint.wantsToDraw;
     this.endGameMessage = savePoint.endGameMessage;
