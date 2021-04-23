dd6e3c5200e967f95a1f7c72a32fcd14ccb45b36
EmmanuelIsaac <58617353+EmmanuelIsaac@users.noreply.github.com>
2021-04-23 15:35:09 +1000

    cycle defines piece mutator

--- src/game/rules/morphlings.ts
+++ src/game/rules/morphlings.ts
@@ -2,10 +2,21 @@ import { PieceName } from "../types";
 import { Rule } from "./CompactRules";
 import { Gait } from "game/types/types";
 
-const PIECE_MUTATOR: { [key in PieceName]?: PieceName } = {
-  [PieceName.Knight]: PieceName.Bishop,
-  [PieceName.Bishop]: PieceName.Knight,
+// PARAM CONSTRAINT: pieces cannot be a member of multiple cycles
+// const PIECE_CYCLES = [[PieceName.Knight, PieceName.Bishop]];
+const PIECE_CYCLES = [[PieceName.Knight, PieceName.Bishop]];
+
+type PieceMutator = { [key in PieceName]?: PieceName };
+const createPieceMutator = (pieceCycles: PieceName[][]): PieceMutator => {
+  return Object.assign(
+    {},
+    ...pieceCycles.flatMap((pieceCycle: PieceName[]) =>
+      pieceCycle.map((p, i, c) => ({ [p]: c[(i + 1) % pieceCycle.length] }))
+    )
+  );
 };
+
+const pieceMutator = createPieceMutator(PIECE_CYCLES);
 
 export const morphlings: Rule = {
   title: "Morphlings",
@@ -15,12 +26,13 @@ export const morphlings: Rule = {
   onPieceDisplaced: ({ board, pieceDelta }) => {
     const piece = board.getPiece(pieceDelta.pieceId);
     if (piece) {
-      const newPieceName = PIECE_MUTATOR[piece.name];
+      const newPieceName = pieceMutator[piece.name];
       if (newPieceName !== undefined) {
         piece.name = newPieceName;
         piece.generateGaits =
           board.interrupt.for.getGaitGenerator({
             name: newPieceName,
+            owner: piece.owner,
           }).gaitGenerator || ((): Gait[] => []);
       }
     }
