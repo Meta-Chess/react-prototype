2db4e0c0ce89d16b6c4bcd2448d19b3cca9cf474
Emmanuel Isaac <epi.misc@gmail.com>
2021-09-17 00:30:01 +1000

    fixed up date months, and added in the newer updates

--- src/components/RootStackNavigator/StartScreen/UpdateLog/updates.ts
+++ src/components/RootStackNavigator/StartScreen/UpdateLog/updates.ts
@@ -1,9 +1,25 @@
 import type { UpdateGroup } from "./UpdateTypes";
 
 export const updates: UpdateGroup[] = [
+  // note js month counting starts from 0 = january
   {
     version: { major: 0, minor: 1, patch: 1 },
-    date: new Date(2021, 9, 10),
+    date: new Date(2021, 8, 16),
+    updates: [
+      {
+        label: "Update",
+        description:
+          "This update log! Access anytime via the help icon in the top right of the home screen",
+      },
+      {
+        label: "Update",
+        description: "Discord splash when public lobby is empty and other minor tweaks",
+      },
+    ],
+  },
+  {
+    version: { major: 0, minor: 1, patch: 1 },
+    date: new Date(2021, 8, 10),
     updates: [
       {
         label: "Variant",
@@ -22,7 +38,7 @@ export const updates: UpdateGroup[] = [
   },
   {
     version: { major: 0, minor: 1, patch: 1 },
-    date: new Date(2021, 9, 9),
+    date: new Date(2021, 8, 9),
     updates: [
       {
         label: "Variant",
@@ -32,7 +48,7 @@ export const updates: UpdateGroup[] = [
   },
   {
     version: { major: 0, minor: 1, patch: 1 },
-    date: new Date(2021, 9, 7),
+    date: new Date(2021, 8, 7),
     updates: [
       {
         label: "Variant",
@@ -46,7 +62,7 @@ export const updates: UpdateGroup[] = [
   },
   {
     version: { major: 0, minor: 1, patch: 1 },
-    date: new Date(2021, 9, 6),
+    date: new Date(2021, 8, 6),
     updates: [
       {
         label: "Update",
