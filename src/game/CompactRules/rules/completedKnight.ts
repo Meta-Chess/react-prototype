cd68bbfefff6af7b74a2d0a4a07419eb788c08a1
Emmanuel Isaac <epi.misc@gmail.com>
2021-10-18 12:12:34 +1100

    feat: completed knight!

---
+++ src/game/CompactRules/rules/completedKnight.ts
@@ -0,0 +1,22 @@
+import { PieceName } from "game/types";
+import { Rule, TrivialParameterRule, OnGaitsGeneratedModify } from "../CompactRules";
+import { range, uniq } from "lodash";
+
+export const completedKnight: TrivialParameterRule = (): Rule => {
+  return {
+    title: "Completed Knight",
+    description: "Knights additionally move 3 spaces in each lateral direction.",
+    onGaitsGeneratedModify: ({ gaits, piece }): OnGaitsGeneratedModify => ({
+      gaits:
+        piece.name === PieceName.Knight
+          ? [
+              ...gaits,
+              ...uniq(gaits.map((gait) => gait.pattern[0])).flatMap((lateralDir) => [
+                { nonBlocking: true, pattern: range(3).map((_i) => lateralDir) },
+              ]),
+            ]
+          : gaits,
+      piece,
+    }),
+  };
+};
