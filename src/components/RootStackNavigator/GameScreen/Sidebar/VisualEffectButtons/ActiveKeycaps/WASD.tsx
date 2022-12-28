0ecde1688497b64e1e874907b7a971209b3000bc
angus <angussbjohnson@gmail.com>
2022-12-28 23:18:56 +1100

    Use 'E' to cycle through board types

--- src/components/RootStackNavigator/GameScreen/Sidebar/VisualEffectButtons/ActiveKeycaps/WASD.tsx
+++ src/components/RootStackNavigator/GameScreen/Sidebar/VisualEffectButtons/ActiveKeycaps/WASD.tsx
@@ -4,7 +4,7 @@ import { Row } from "ui";
 import { KeyCap } from "./KeyCap";
 import { SFC } from "primitives";
 import { GameContext } from "components/shared";
-import { isCircularBoardPossible } from "components/shared/Board/useCircularBoard";
+import { getPossibleBoards } from "components/shared/Board/useBoardType";
 
 export const WASD: SFC = ({ style }) => {
   const { gameMaster } = useContext(GameContext);
@@ -14,17 +14,18 @@ export const WASD: SFC = ({ style }) => {
   const verticalRotationAllowed = !!gameMaster
     ?.getRuleNames()
     .includes("verticallyCylindrical");
+  const multipleBoardsAllowed = getPossibleBoards(gameMaster).length > 1;
 
-  const circularBoardAllowed = isCircularBoardPossible(gameMaster);
-  if (!horizontalRotationAllowed && !verticalRotationAllowed && !circularBoardAllowed)
+  if (!horizontalRotationAllowed && !verticalRotationAllowed && !multipleBoardsAllowed)
     return null;
 
+  // TODO: WASD should be disabled when showing 3D boards (until 3D boards use WASD)
   return (
     <View style={style}>
       <Row>
         <KeyCap />
         <KeyCap letter={"W"} active={verticalRotationAllowed} />
-        <KeyCap letter={"E"} active={circularBoardAllowed} />
+        <KeyCap letter={"E"} active={multipleBoardsAllowed} />
       </Row>
       <Row>
         <KeyCap letter={"A"} active={horizontalRotationAllowed} />
---
+++ src/components/shared/Board/useBoardType.ts
@@ -0,0 +1,69 @@
+import { useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
+import { GameMaster } from "game";
+import { GameContext } from "components/shared";
+import { uniq } from "lodash";
+
+type BoardType = "flat" | "spherical" | "circular";
+
+export const getPossibleBoards = (gameMaster?: GameMaster): BoardType[] => {
+  const possibleBoards: BoardType[] = ["flat"];
+  if (gameMaster?.getRuleNames().includes("verticallyCylindrical"))
+    possibleBoards.push("circular");
+  if (gameMaster?.getRuleNames().includes("polar")) possibleBoards.push("spherical");
+  return uniq(possibleBoards);
+};
+
+export const useBoardType = (boardTypeOverride?: BoardType): BoardType => {
+  const { gameMaster } = useContext(GameContext);
+
+  const possibleBoards = useMemo(
+    () => getPossibleBoards(gameMaster),
+    [gameMaster?.getRuleNames()]
+  );
+  const defaultBoardType = useMemo(
+    (): BoardType =>
+      gameMaster?.getRuleNames().includes("longBoard") &&
+      gameMaster?.getRuleNames().includes("verticallyCylindrical")
+        ? "circular"
+        : gameMaster?.getRuleNames().includes("polar")
+        ? "spherical"
+        : "flat",
+    [gameMaster?.getRuleNames()]
+  );
+
+  const [boardType, setBoardType] = useState<BoardType>(defaultBoardType);
+  const pressRef = useRef<BoardType>(defaultBoardType);
+
+  // TODO: extract this keypressing logic from here (and other places)
+  const onKeypressEvent = useCallback((event) => {
+    switch (event.key) {
+      case "e":
+      case "E":
+        if (possibleBoards.length > 1) {
+          pressRef.current = nextBoard(pressRef.current, possibleBoards); // relies on uniqueness of possible boards
+          gameMaster?.unselectAllPieces();
+          gameMaster?.render();
+        }
+        break;
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keypress", onKeypressEvent, false);
+    return (): void => {
+      document.removeEventListener("keypress", onKeypressEvent, false);
+    };
+  }, []);
+
+  useEffect(() => {
+    setBoardType(pressRef.current);
+  }, [pressRef.current]);
+
+  return boardTypeOverride ?? boardType;
+};
+
+function nextBoard(currentBoard: BoardType, possibleBoards: BoardType[]): BoardType {
+  return possibleBoards[
+    (possibleBoards.findIndex((x) => x === currentBoard) + 1) % possibleBoards.length
+  ];
+}
