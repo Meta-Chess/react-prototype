565ae63a48d147416918769cf87b0ae3f7220e1e
angus <angussbjohnson@gmail.com>
2022-12-31 15:27:53 +1100

    Extract `Lighting`

---
+++ src/components/shared/Board/Lighting.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import { Colors } from "primitives";
+
+export const Lighting: FC = () => {
+  // general glow plus three slightly different-warmth lights around the equator
+  return (
+    <>
+      <ambientLight intensity={0.45} />
+      <directionalLight
+        position={[0, 0, 20]}
+        color={Colors.LIGHTING.WARM.toString()}
+        castShadow={true}
+      />
+      <directionalLight
+        position={[17, 0, -10]}
+        color={Colors.LIGHTING.COLD.toString()}
+        castShadow={true}
+      />
+      <directionalLight
+        position={[-17, 0, -10]}
+        color={Colors.LIGHTING.NEUTRAL.toString()}
+        castShadow={true}
+      />
+    </>
+  );
+};
