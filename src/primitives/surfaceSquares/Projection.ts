c49661b9b00f2348d151da3c1805b499392416a8
Angus Leck <aleck42@gmail.com>
2022-12-31 18:49:49 +1100

    remove unused files

--- src/primitives/surfaceSquares/Projection.ts
+++
@@ -1,9 +0,0 @@
-import { Vector3 } from "three";
-
-export type Projection = (boardCoordinates: {
-  file: number;
-  rank: number;
-  numberOfFiles: number;
-  numberOfRanks: number;
-  heightAdjustment?: number;
-}) => { position: Vector3; normal: Vector3 };
