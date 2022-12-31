1c951e212fae64dfd876517ac6a84cf9fd4e20b6
angus <angussbjohnson@gmail.com>
2022-12-31 11:15:57 +1100

    Modal message to remind users "Press E to change view"

--- src/components/shared/Board/Board.tsx
+++ src/components/shared/Board/Board.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useEffect, useRef } from "react";
+import { Animated, Platform } from "react-native";
 import { SquareShape, TokenName } from "game";
 import { HexBoard } from "./HexBoard";
 import { BoardMeasurements } from "./calculateBoardMeasurements";
@@ -7,6 +8,8 @@ import { CircularBoard } from "./CircularBoard";
 import { useBoardType } from "./useBoardType";
 import { SquareBoard } from "./SquareBoard";
 import { Board3D } from "./Board3D";
+import { Colors, Text } from "primitives";
+import { AbsoluteView } from "ui";
 
 export interface BoardProps {
   backboard?: boolean;
@@ -19,6 +22,21 @@ export const Board: FC<BoardProps> = (props) => {
   const { gameMaster } = useContext(GameContext);
   const shapeToken = gameMaster?.game.board.firstTokenWithName(TokenName.Shape);
 
+  // Animated modal opacity
+  const modalOpacity = useRef(new Animated.Value(0.75)).current;
+  useEffect(() => {
+    // modalOpacity.setValue(0.5);
+    Animated.sequence([
+      Animated.delay(500),
+      Animated.timing(modalOpacity, {
+        toValue: 0,
+        duration: 1500,
+        isInteraction: false,
+        useNativeDriver: Platform.OS === "ios",
+      }),
+    ]).start();
+  }, []);
+
   const boardTypeOverride =
     props.circularBoard === true
       ? "circular"
@@ -26,7 +44,7 @@ export const Board: FC<BoardProps> = (props) => {
       ? "flat"
       : undefined;
 
-  const boardType = useBoardType(boardTypeOverride);
+  const { boardType, possibleBoards } = useBoardType(boardTypeOverride);
 
   return (
     <>
@@ -38,6 +56,22 @@ export const Board: FC<BoardProps> = (props) => {
         <SquareBoard {...props} />
       ) : (
         <Board3D {...props} type={boardType} />
+      )}
+      {possibleBoards.length > 1 && (
+        <AbsoluteView pointerEvents="none">
+          <Animated.View
+            style={{
+              width: 240,
+              padding: 16,
+              backgroundColor: Colors.DARKER.toString(),
+              opacity: modalOpacity,
+              borderRadius: 12,
+            }}
+            pointerEvents="none"
+          >
+            <Text alignment={"center"}>Press E to change view</Text>
+          </Animated.View>
+        </AbsoluteView>
       )}
     </>
   );
