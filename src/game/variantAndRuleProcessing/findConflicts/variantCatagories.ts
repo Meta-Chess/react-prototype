f3095ed08c322a4e9a934a4b787361d99c8140fc
eman <eman.makes.stuff@gmail.com>
2023-05-10 09:22:24 +1000

    feat: adding in conflicts for grand (and other clashing piece setups)
    may restructure how these setup conflicts are done if there are more non-mutually exclusive cases like the long boards

--- src/game/variantAndRuleProcessing/findConflicts/variantCatagories.ts
+++ src/game/variantAndRuleProcessing/findConflicts/variantCatagories.ts
@@ -1,8 +1,13 @@
-import { FutureVariantName } from "game/variants";
+import { FutureVariantName, futureVariants } from "game/variants";
+import { keys } from "utilities";
 
 export type VariantCatagories = {
   [group in string]: FutureVariantName[];
 };
+
+const BOARD_IS_LONG = keys(futureVariants).filter((variantName) =>
+  futureVariants[variantName].ruleNames.includes("longBoard")
+);
 
 export const VARIANT_CATAGORIES: VariantCatagories = {
   triggersCheckCaringAboutPassiveMoves: ["chemicallyExcitedKnight"],
@@ -11,7 +16,8 @@ export const VARIANT_CATAGORIES: VariantCatagories = {
   boardCantHavePoles: ["hex", "toroidal", "mobius", "kleinBottle"],
   boardHasPoles: ["polar", "spherical"],
   boardCantBeLong: ["hex"],
-  boardIsLong: ["toroidal", "mobius", "kleinBottle"],
+  boardIsLong: BOARD_IS_LONG,
+  mutuallyExclusiveBoardSetups: ["grandChess", "hex"],
   noFork: ["noFork"],
   thatCanMakeNoForkStalemate: ["fatigue"],
   combinatoricHexCylinderPull: ["hex", "cylindrical", "pull"],
