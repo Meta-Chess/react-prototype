77524df3287845a8e627282fbf49b5335361427d
eman <eman.makes.stuff@gmail.com>
2025-05-09 08:11:22 +1000

    changing the share link based on which space you are in
    e.g. mchess.io/nimbus/game/XYZ for Nimbus over mchess.io/game/XYZ

--- src/components/RootStackNavigator/GameScreen/Sidebar/RoomIdCard.tsx
+++ src/components/RootStackNavigator/GameScreen/Sidebar/RoomIdCard.tsx
@@ -3,13 +3,24 @@ import { Colors, LinkIcon, SFC } from "primitives";
 import { Block } from "ui";
 import copy from "copy-to-clipboard";
 import { IconButton } from "ui/Buttons/IconButton";
+import { Screens, useRoute } from "navigation";
+import { getSpaceFromRouteName } from "navigation/Spaces";
 
 interface Props {
   roomId?: string;
 }
 
+const getShareUrl = (routeName: Screens, roomId: string): string => {
+  const baseUrl = "mchess.io";
+  const space = getSpaceFromRouteName(routeName);
+  return space ? `${baseUrl}/${space}/game/${roomId}` : `${baseUrl}/game/${roomId}`;
+};
+
 const RoomIdCard: SFC<Props> = ({ roomId, style }) => {
+  const route = useRoute();
+
   if (!roomId) return null;
+
   return (
     <Block
       title={`Invite key: ${roomId}`}
@@ -18,7 +29,7 @@ const RoomIdCard: SFC<Props> = ({ roomId, style }) => {
           Icon={LinkIcon}
           color={Colors.WHITE.fade(0.4)}
           onPress={(): void => {
-            copy(`mchess.io/game/${roomId}`);
+            copy(getShareUrl(route.name, roomId));
           }}
         />
       }
